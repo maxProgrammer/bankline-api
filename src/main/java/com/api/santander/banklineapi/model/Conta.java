f64fdb3984ee25b3a0afb7165c70534b5e6739ea
maxProgrammer <maxwneto.poo@gmail.com>
2022-05-04 15:12:08 -0300

    mapeando entididades com JPA

--- src/main/java/com/api/santander/banklineapi/model/Conta.java
+++ src/main/java/com/api/santander/banklineapi/model/Conta.java
@@ -1,8 +1,13 @@
 package com.api.santander.banklineapi.model;
 
-public class Conta {
+import javax.persistence.*;
 
+
+@Embeddable
+public class Conta {
+    @Column(name = "conta_numero")
     private Long numero;
+    @Column(name = "conta_saldo")
     private Double saldo;
 
     public Long getNumero() {
